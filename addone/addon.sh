cbedbc7407e1f3d66a1e403845b1739990235653
Mike Lei <mikelei8291@users.noreply.github.com>
2015-11-01 22:06:46 +0800

    Delete addon.sh
    Already add in install-nginx.sh

--- addone/addon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tar zxvf libresssl-2.3.0.tar.gz
-mkdir -p /usr/local/nginx/modules
-tar xvfvz v1.9.32.10-beta.tar.gz -C /usr/local/nginx/modules --no-same-owner
-tar xvfvz 1.9.32.10.tar.gz -C /usr/local/nginx/modules/ngx_pagespeed-1.9.32.10-beta --no-same-owner
-find /usr/local/nginx/modules/ngx_pagespeed-1.9.32.10-beta/ -type d -exec chmod +rx {} \;
-find /usr/local/nginx/modules/ngx_pagespeed-1.9.32.10-beta/ -type f -exec chmod +r {} \;
