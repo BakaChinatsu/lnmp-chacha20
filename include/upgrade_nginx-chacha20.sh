085a0843a8f92a5adb5c8b6bbf8d4704b56dc7a8
雨宮千夏 <hanzexu990323@gmail.com>
2015-11-09 07:38:18 +0900

    update

--- include/upgrade_nginx-chacha20.sh
+++ include/upgrade_nginx-chacha20.sh
@@ -16,13 +16,14 @@ Upgrade_Nginx()
     fi
 
     Nginx_Version=""
-    Pgs_Version=""
     echo "Current Nginx Version:${Cur_Nginx_Version}"
     echo "You can get version number from http://nginx.org/en/download.html"
     read -p "Please enter nginx version you want, (example: 1.9.5 ): " Nginx_Version
     if [ "${Nginx_Version}" = "" ]; then
         echo "Error: You must enter a nginx version!!"
         exit 1
+
+     Pgs_Version=""
     if [ "${Nginx_Version}" > "1.9.5" ]; then
         echo "Current ngx_pagespeed Version:${Cur_Pgs_Version}"
         echo "You can get version number from https://github.com/pagespeed/ngx_pagespeed/"
